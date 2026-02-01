9d01cea707af714fc000ba601c1d68317ac8035f
Muntadhre Alsairafi <m.alsairafi03@gmail.com>
2026-02-01 17:25:58 +0300

    product feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRoute from "./routes/userRoutes"
+import userRoute from "./routes/userRoute"
+import { seedInitialProducts } from "./services/productService";
+import productRoute from "./routes/productRoute";
 
 const app = express();
 const port = 3001;
@@ -12,9 +14,12 @@ mongoose
   .then(() => console.log("mongo connected!"))
   .catch((err)=> console.log("Failed connection",err));
 
+// seed the products to databse
+seedInitialProducts();
 
-app.use("/user", userRoute)
+app.use("/user", userRoute);
+app.use("/product", productRoute)
 
   app.listen(port,()=>{
-    console.log(`Server is running at: http//localhost:${port}`)
+    console.log(`Server is running at: http://localhost:${port}`)
   })
---
+++ src/services/productService.ts
@@ -0,0 +1,17 @@
+import productModel from "../models/productModel";
+
+export const getAllProducts = async () =>{
+    return await productModel.find();
+};
+
+export const seedInitialProducts = async () => {
+    const products =[
+        {title: "Dell Laptob", image: "https://www.dell.com/wp-uploads/2024/01/XPS-9640-laptops-back-to-back-1280x800-1.jpg", price:350, stock:100 },
+    ];
+
+    const existingProducts = await getAllProducts();
+
+    if(existingProducts.length === 0) {
+        await productModel.insertMany(products)
+    }
+};
